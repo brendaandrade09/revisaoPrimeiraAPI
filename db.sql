75cc52e832b8d7e1702e2ccce72571d3c316aef0
BRENDA LIMA ANDRADE <26171605@edu119.local>
2026-09-15 14:51:51 -0300

    Opa

---
+++ db.sql
@@ -0,0 +1,14 @@
+SHOW DATABASES;
+CREATE DATABASE turmads1b;
+CREATE TABLE alunos(
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+curso VARCHAR(100) NOT NULL
+);
+SHOW TABLES;
+SELECT * FROM alunos;
+INSERT INTO alunos (nome, curso)
+VALUES ('Meel', 'Direito'),
+('Matt', 'Mecatrônica'),
+('Jack', 'Administração'),
+('Luk', 'Desenvolvimento de sistemas');
