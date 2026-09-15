fce4cb4d1f0d250aac4013844f1c6b154d302883
BRENDA LIMA ANDRADE <26171605@edu119.local>
2026-09-15 10:30:47 -0300

    Atualizandoooo

--- server.js
+++ server.js
@@ -10,3 +10,28 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// {
+//     "nome": "Beren",
+//     "curso": "Desenvolvimento de sistemas"
+// }
+
+let ALUNOS = [
+    {id: 1, nome: "Meel", curso: "Direito"},
+    {id: 2, nome: "Matt", curso: "Mecatrônica"},
+    {id: 3, nome: "Jack", curso: "Administração"},
+    {id: 4, nome: "Luk", curso: "Desenvolvimento de sistemas"},
+]
+
+app.get("/",(req,res)=>{
+    res.json({
+        mensagem: "API alunos funcionando!"
+    })
+})
+
+const PORTA = 3000;
+
+app.listen(PORTA,()=>{
+    console.log(`Servidor iniciado com sucesso!`);
+    console.log(`http://localhost:${`${PORTA}`}`);
+})
