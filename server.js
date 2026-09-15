667a2d3412d299fe2b8e4c7486b0ef333b4ca3f2
BRENDA LIMA ANDRADE <26171605@edu119.local>
2026-09-15 11:17:40 -0300

    Atualização 02 (eu acho)

--- server.js
+++ server.js
@@ -17,25 +17,45 @@ app.use(express.json());
 // }
 
 let ALUNOS = [
-    {id: 1, nome: "Meel", curso: "Direito"},
-    {id: 2, nome: "Matt", curso: "Mecatrônica"},
-    {id: 3, nome: "Jack", curso: "Administração"},
-    {id: 4, nome: "Luk", curso: "Desenvolvimento de sistemas"},
+    { id: 1, nome: "Meel", curso: "Direito" },
+    { id: 2, nome: "Matt", curso: "Mecatrônica" },
+    { id: 3, nome: "Jack", curso: "Administração" },
+    { id: 4, nome: "Luk", curso: "Desenvolvimento de sistemas" },
 ]
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.json({
         mensagem: "API alunos funcionando!"
     })
 })
 
-app.get("/alunos",(req,res)=>{
+app.get("/alunos", (req, res) => {
     res.json(ALUNOS);
 });
 
+app.get("/alunos/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const aluno = ALUNOS.find(aluno => aluno.id === id);
+
+if(!aluno){
+    return res.status(404).json({
+        mensagem: "Aluno não encontrado."
+    });
+}
+
+
+app.post("/alunos/cadastrar",(req,res)=>{
+
+});
+
+    res.status(200).json(aluno);
+    // console.log(req);
+    res.send("Funcionando");
+})
+
 const PORTA = 3000;
 
-app.listen(PORTA,()=>{
+app.listen(PORTA, () => {
     console.log(`Servidor iniciado com sucesso!`);
     console.log(`http://localhost:${`${PORTA}`}`);
 })
